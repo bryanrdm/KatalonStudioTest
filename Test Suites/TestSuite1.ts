5f17b566ec47d16f28db4ccf34e1aac511760430
bryanrdm <burhanerdem@gmail.com>
2020-09-18 10:04:17 -0400

    corrected mistakes

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8d6eb22b-b6ca-4d21-bee8-0511b6b0ec10</testSuiteGuid>
+</TestSuiteEntity>
